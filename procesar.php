95be2fcca22fa7890743ba5a4376ccd2154bdc9a
sans1960 <asanscliment@gmail.com>
2021-08-19 18:30:48 +0200

    corrections

--- procesar.php
+++ procesar.php
@@ -1,4 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proceso</title>
+    <link rel="stylesheet" href="formulari.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <div class="proceso">
+
+    <?php
 if(isset($_POST['send'])){
     $name = $_POST['name'];
     $surname = $_POST['surname'];
@@ -12,6 +27,8 @@ if(isset($_POST['send'])){
     $travellers = $_POST['travellers'];
     $triptype = $_POST['triptype'];
     $specifications = $_POST['specifications'];
+    $children =$_POST['children'];
+    $destinity = $_POST['destinity'];
     $message = $_POST['message'];
 
     // $data = file_get_contents('contactes.json');
@@ -31,6 +48,8 @@ if(isset($_POST['send'])){
         'travellers' => $_POST['travellers'],
         'triptype' => $_POST['triptype'],
         'specifications' => $_POST['specifications'],
+        'children'=>$_POST['children'],
+        'destinity'=>$_POST['destinity'],
         'message' => $_POST['message'],
     );
     $data[] = $input;
@@ -38,7 +57,50 @@ if(isset($_POST['send'])){
      file_put_contents('contact'.$name.'.'.'json', $data);
     
 
-     echo "contact saved";
+     echo "<div class='alert'>";
+     echo "<p>Thanks you : ".$name." ".$surname." "."</p>";
+     echo "<p>We will contact with you</p>";
+     echo "<p>We will recived a email</p>";
 
+     echo "</div>";
+
+    
+
+     $to = "$email, g.sans.real@gmail.com";
+     $subject = "Contact for trip";
+     $headers = "MIME-Version: 1.0" . "\r\n";
+     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+     $message ="Thanks you : ".$name." ".$surname." "."</p>";
+     mail($to, $subject, $message, $headers);  
+
+
+     echo "<div class='enlace'>";
+     echo "<a href='formulario.php'>Back</a>";
+     echo "</div>";
 }
+?>
+    
+
+    </div>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
