8f262b143752fdf8604da10afab5e7b8e1362e49
sans1960 <asanscliment@gmail.com>
2021-08-22 09:39:09 +0200

    plantilla email

--- procesar.php
+++ procesar.php
@@ -81,12 +81,76 @@ if(isset($_POST['send'])){
 
     
 
-     $to = "$email, g.sans.real@gmail.com";
+     $to = $email;
      $subject = "Contact for trip";
      $headers = "MIME-Version: 1.0" . "\r\n";
      $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
      $message ="Thanks you : ".$name." ".$surname." "."</p>";
      mail($to, $subject, $message, $headers);  
+
+    $htmlContent = '<html lang="en">
+    <head>
+    
+        <title>Email</title>
+        <style>
+            *{
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+            }
+            h1{
+                text-align:center ;
+            }
+            table{
+                margin: 20px auto;
+                padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Welcome to Soujourplanet </h1>
+    <table>
+        <tr>
+            <th>Name : </th><td>'.$name.'</td>
+        </tr>
+        <tr>
+            <th>Surname : </th><td>'.$surname.'</td>
+        </tr>
+        <tr>
+            <th>Email : </th><td>'.$email.'</td>
+        </tr>
+        <tr>
+            <th>Phone : </th><td>'.$phone.'</td>
+        </tr>
+        <tr>
+            <th>City : </th><td>'.$city.'</td>
+        </tr>
+        <tr>
+            <th>State : </th><td>'.$state.'</td>
+        </tr>
+        <tr>
+            <th>Zipcode : </th><td>'.$zipcode.'</td>
+        </tr>
+        <tr>
+            <th>Duration : </th><td>'.$duration.'</td>
+        </tr>
+        <tr>
+            <th>Season : </th><td>'.$season.'</td>
+        </tr>
+        <tr>
+            <th>Tavellers : </th><td>'.$travellers.'</td>
+        </tr>
+        <tr>
+            <th>Triptype : </th><td>'.$triptype.'</td>
+        </tr> 
+    </table>
+    </body>
+    </html>';
+    mail('g.sans.real@gmail.com',$subject,$htmlContent,$headers);
+
+
+
+
+
+
 
 
      echo "<div class='enlace'>";
